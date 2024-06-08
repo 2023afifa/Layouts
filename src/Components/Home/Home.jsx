bfd9ae4a7c5e8e862bc6d4d8455dd8c7fdc67a9d
afifa <2023afifa@gmail.com>
2024-06-08 17:05:58 +0600

    Home navbar button changed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <div className="bg-slate-300 min-h-screen">
-            <div className="text-center pt-14 mb-10">
+            <h1 className="text-center text-3xl font-bold py-10">All My Layouts</h1>
+            <div className="text-center mb-10">
                 <Link to="/nav">
-                    <button className="btn bg-indigo-500 text-slate-200 normal-case hover:bg-indigo-400">Navbar</button>
+                    <button className="px-20 py-10 rounded bg-indigo-500 text-slate-200 text-2xl font-semibold normal-case hover:bg-indigo-600">Navbar</button>
                 </Link>
             </div>
         </div>
